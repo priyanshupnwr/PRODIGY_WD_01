fdba247babf6c11d42a6a66e2e5f041b7889333f
priyanshupnwr <priyanshupanwarrr@gmail.com>
2026-07-17 09:19:56 +0530

    improvements

--- script.js
+++ script.js
@@ -1,25 +1,33 @@
 const navbar = document.querySelector(".navbar");
 const menuBtn = document.querySelector(".menu-btn");
+const menuIcon = menuBtn.querySelector("i");
 const navLinks = document.querySelector(".nav-links");
+const links = document.querySelectorAll(".nav-links a");
 
 window.addEventListener("scroll", () => {
-
     if (window.scrollY > 50) {
         navbar.classList.add("scrolled");
     } else {
         navbar.classList.remove("scrolled");
     }
-
 });
 
 menuBtn.addEventListener("click", () => {
     navLinks.classList.toggle("active");
+
+    if (navLinks.classList.contains("active")) {
+        menuIcon.classList.remove("fa-bars");
+        menuIcon.classList.add("fa-xmark");
+    } else {
+        menuIcon.classList.remove("fa-xmark");
+        menuIcon.classList.add("fa-bars");
+    }
 });
 
-document.querySelectorAll(".nav-links a").forEach(link => {
-
+links.forEach(link => {
     link.addEventListener("click", () => {
         navLinks.classList.remove("active");
+        menuIcon.classList.remove("fa-xmark");
+        menuIcon.classList.add("fa-bars");
     });
-
 });
